711b142df6d86cf3663433aed2e8eafd506868c4
Ronak Gupta <rgupta@Ronaks-MacBook-Air.local>
2026-04-07 20:33:30 +0530

    Create APIG & Lambda

--- Terrafrom/agtw_visitorcount.tf
+++
@@ -1,4 +0,0 @@
-## resource "aws_apigatewayv2_api" "visitor_api" {
-##   name          = "visitor-api"
-##   protocol_type = "HTTP"
-## }
--- Terrafrom/rds_dynomo.tf
+++ Terrafrom/rds_dynomo.tf
@@ -1,10 +1,12 @@
-## resource "aws_dynamodb_table" "visitor_counter" {
-##   name         = "visitor-counter"
-##   billing_mode = "PAY_PER_REQUEST"
-##   hash_key     = "id"
-## 
-##   attribute {
-##     name = "id"
-##     type = "S"
-##   }
-## }
+resource "aws_dynamodb_table" "visitor_counter" {
+  name         = "VisitorCounter"
+  billing_mode = "PAY_PER_REQUEST"
+
+  hash_key    = "counterId"
+  table_class = "STANDARD"
+
+  attribute {
+    name = "counterId"
+    type = "S"
+  }
+}
